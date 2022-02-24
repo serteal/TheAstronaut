b4f91df36d5c893a6215de612c4f5698756e2a24
40022tsa <4002tsa@gmail.com>
2022-02-24 11:35:17 +0100

    responsivity?

--- script.js
+++ script.js
@@ -888,14 +888,15 @@ loader.load("https://raw.githubusercontent.com/baronwatts/models/master/moon-veh
 class JoyStick {
   constructor(options) {
     const circle = document.createElement("div");
-    circle.style.cssText = "position:absolute; bottom:35px; width:80px; height:80px; background:rgba(126, 126, 126, 0.5); border:#444 solid medium; border-radius:50%; left:50%; transform:translateX(-50%);";
+    //circle.style.cssText = "";
     const thumb = document.createElement("div");
-    thumb.style.cssText = "position: absolute; left: 20px; top: 20px; width: 40px; height: 40px; border-radius: 50%; background: #fff;";
+    //thumb.style.cssText = "";
+    thumb.className = "thumb"
     circle.appendChild(thumb);
-    circle.className = "joystick";
+    circle.className = "circle";
     document.body.appendChild(circle);
     this.domElement = thumb;
-    this.maxRadius = options.maxRadius || 40;
+    this.maxRadius = options.maxRadius || 55;
     this.maxRadiusSquared = this.maxRadius * this.maxRadius;
     this.onMove = options.onMove;
     this.game = options.game;
